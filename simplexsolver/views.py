ae1b44ab5b2a549bcb66d349df3cbcf40a6f2123
kwabenankyi <anthony.nkyi@outlook.com>
2024-01-07 16:21:01 +0000

    updated views

--- simplexsolver/views.py
+++ simplexsolver/views.py
@@ -10,17 +10,17 @@ def index(request):
         formset2 = ConstraintsFormSet(request.POST, prefix="formset2")
         if formset1.is_valid() and formset2.is_valid():
             d = request.POST.dict()
-            maxmin=d['maxmin']
             objectivefunction=d['formset1-objective_function']
             constraints=[]
             numofconstraints = (len(d)-7)//2
+            #validation for constraints
             for i in range(numofconstraints):
                 newcon = d['formset2-'+str(i)+'-constraint']
                 if ('<=' in newcon) or ('>=' in newcon):
                     constraints.append(d['formset2-'+str(i)+'-constraint'])
                 else:
                     return render(request, "index.html", {'valid':False, 'formset1': formset1, 'formset2': formset2})
-            return solution(request,maxmin,objectivefunction,constraints)
+            return solution(request,d['maxmin'],objectivefunction,constraints)
     else:
         formset1 = ObjectiveFunctionForm(prefix="formset1")
         formset2 = ConstraintsFormSet(prefix="formset2")
