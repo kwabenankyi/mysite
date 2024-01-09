25b993c0f101e19528691a33f6bea4e30a625993
kwabenankyi <anthony.nkyi@outlook.com>
2024-01-09 01:11:56 +0000

    added placeholders to text

--- simplexsolver/forms.py
+++ simplexsolver/forms.py
@@ -5,6 +5,6 @@ class ObjectiveFunctionForm(forms.Form):
     objective_function = forms.CharField(max_length=200, required=True)
 
 class ConstraintForm(forms.Form):
-    constraint = forms.CharField(max_length=200, required=True)
+    constraint = forms.CharField(max_length=200, required=True, widget=forms.TextInput(attrs={'placeholder':'e.g. x+y<=10 or 2w-z>=-4'}))
 
 ConstraintsFormSet = forms.formset_factory(ConstraintForm, max_num=2, min_num=2, validate_min=True)
